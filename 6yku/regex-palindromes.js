99014060ec67630f2c287763c1bc5f1a9b26ed32
JamesRiall <jamesrariall@gmail.com>
2017-09-24 11:07:10 +0100

    added 7kyu files

---
+++ 6yku/regex-palindromes.js
@@ -0,0 +1,35 @@
+// https://www.codewars.com/kata/573e6831e3201f6a9b000971
+
+// This time your task is to write a regular expressions matching all palindrome substring of a string.
+
+// The rules:
+
+// 1) The string contains letters, numbers, underscores and spaces. space is the separator. the substring means the whole of a substring that separated by spaces. For example:
+// ```
+// "aaa bcccd" should match a substring "aaa" ,
+// should not match "ccc", "ccc" is a part of substring "bcccd"
+// ```
+
+// 2) Palindrome substring length range is 2-7. It means don't match the substring which length=1 or length>7.
+
+// 3) A string may contain multiple palindrome substring.
+
+// 4) You should not ignore case.
+
+// 5) Your regular expression name should be ```regex``` and your result should be a string array.
+
+// Some examples:
+// ```
+// "aa bbb cccc".match(regex)             should return [ 'aa', 'bbb', 'cccc' ]
+// "aaa bcccd".match(regex)               should return [ 'aaa' ]
+// "_x_x_ --- ~~|~~".match(regex)         should return [ '_x_x_' ]
+// "ABCDCBA ABABABA".match(regex)         should return [ 'ABCDCBA', 'ABABABA' ]
+// "121 1221 13577531 11211".match(regex) should return [ '121', '1221', '11211' ]
+// "aabbbcccc d".match(regex)             should return null
+// "1    1".match(regex)                  should return null
+// "abbA CdDc".match(regex)               should return null
+// ```
+
+// SOLUTION
+
+var regex= /\b(\w)(\w)?(\w)?(\w)?\3\2\1\b/g;
