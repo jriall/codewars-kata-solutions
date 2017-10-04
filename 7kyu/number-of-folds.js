a4991aa4327733cfcf8d3e8173c45716df66186e
JamesRiall <jamesrariall@gmail.com>
2017-10-04 07:20:46 +0100

    added number-of-folds 7kyu solution

---
+++ 7kyu/number-of-folds.js
@@ -0,0 +1,27 @@
+// https://www.codewars.com/kata/number-of-folds/train/javascript
+
+// Suppose I have a big paper and i want to draw n number of grids just by folding it (see example ). How many times do i have to fold it to get n number of grids. eg.
+
+// n=1 , 0 operation, the original paper is one grid.
+
+// n=2, 1 operations, i have to fold it 1 time to get 2 grids (A,B).
+
+//  _______
+// |   |   |
+// | A | B |
+// |   |   |
+// |   |   |
+// |___|___|
+// No edge cases, all inputs are valid, But some built-in Math functions will have NO power here.
+
+// SOLUTION
+
+function numberOfFolds(n){
+  let grids = 1;
+  let folds = 0;
+  while (grids < n) {
+    grids*=2;
+    folds++;
+  }
+  return folds;
+}
