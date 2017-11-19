6e6a8430d955dda3fbe1cec6ab5cc0507b2bf6de
JamesRiall <jamesrariall@gmail.com>
2017-11-19 10:41:19 +0000

    added kind remainder 7kyu solution

---
+++ 7kyu/kind-remainder.js
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/kind-rem-a-inder/javascript
+
+// Write a function that will receive three arguments. The first two will always be arrays consisting of integers, the third one will be a string of letters. The lengths of the first two arrays will be equal, and the integers will always be positive.
+
+// You function should divide the first integer of the first array by the first one of the second array to find the remainder and then do the same with the second pair of integers, etc. If the biggest remainder of all found is an even number, the function should return the string in minuscule (small letters). Otherwise, the function should return the string in majuscule (capital letters).
+
+// Example:
+
+// function([16, 16], [10, 5], 'Doctor') should return 'doctor', because:
+
+// the remainder of the first pair of integers (16 divided by 10) is 6;
+
+// the remainder of the second pair is 1;
+
+// the biggest remainder found is 6, and since 6 is an even number --> the string is returned in minuscule.
+
+// SOLUTION
+
+function kira(a, b, c) {
+  return Math.max(...a.map((a,i)=>a%b[i]))%2===1?c.toUpperCase():c.toLowerCase();
+}
