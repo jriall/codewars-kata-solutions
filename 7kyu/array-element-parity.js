d7c92c330f46e2b6e32b5010515bc24c4e472959
JamesRiall <jamesrariall@gmail.com>
2017-11-24 19:52:53 +0000

    added array element parity

---
+++ 7kyu/array-element-parity.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/array-element-parity/javascript
+
+// In this Kata, you will be given an array of integers whose elements have both a negative and a positive value, except for one integer that is either only negative or only positive. Your task will be to find that integer.
+
+// For example,
+
+// solve[1,-1,2,-2,3] = 3  --> 3 only has a positive ocurrence.
+// solve([-3,1,2,3,-1,-4,-2]) = -4  --> -4 only has a negative occurence
+// solve([1,-1,2,-2,3,3]) = 3  --> the integer that is only positive or only negative my appear more than once.
+
+// SOLUTION
+
+function solve(arr){
+  return arr.filter(a=>arr.indexOf(-a)===-1)[0];
+}
