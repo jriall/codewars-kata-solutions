e927b77851a4f86997b72176380212ca2fc45902
JamesRiall <jamesrariall@gmail.com>
2017-11-27 08:26:26 +0000

    added rgb to hex 5kyu solution

---
+++ 5kyu/rgb-to-hex-conversion.js
@@ -0,0 +1,25 @@
+// https://www.codewars.com/kata/rgb-to-hex-conversion/javascript
+
+// The rgb() method is incomplete. Complete the method so that passing in RGB decimal values will result in a hexadecimal representation being returned. The valid decimal values for RGB are 0 - 255. Any (r,g,b) argument values that fall out of that range should be rounded to the closest valid value.
+
+// The following are examples of expected output values:
+
+// rgb(255, 255, 255) // returns FFFFFF
+// rgb(255, 255, 300) // returns FFFFFF
+// rgb(0,0,0) // returns 000000
+// rgb(148, 0, 211) // returns 9400D3
+
+// SOLUTION
+
+function toHex(n) {
+ n = parseInt(n,10);
+ if (isNaN(n)) return "00";
+ n = Math.max(0,Math.min(n,255));
+ return "0123456789ABCDEF".charAt((n-n%16)/16)
+      + "0123456789ABCDEF".charAt(n%16);
+}
+
+function rgb(r, g, b){
+  return `${toHex(r)}${toHex(g)}${toHex(b)}`;
+}
+
